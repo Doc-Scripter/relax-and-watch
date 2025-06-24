52b8d77a1ce0fc54de4910017713ea731e5fca04
Doc-Scripter <clifford242526@gmail.com>
2025-06-24 15:31:52 +0300

    feat(tmdb): add new methods for movie credits, genres, search and discover
    Implement additional TMDB API endpoints to fetch movie credits, genres, and enable searching/discovering movies with filters. Also fix JSON unmarshal formatting.

--- backend/internal/api/tmdb.go
+++ backend/internal/api/tmdb.go
@@ -53,6 +53,67 @@ func (c *TMDBClient) GetTrendingMovies() ([]interface{}, error) {
 	return nil, fmt.Errorf("could not find 'results' array in TMDB trending response")
 }
 
+// GetMovieCredits fetches cast and crew information for a movie from TMDB.
+func (c *TMDBClient) GetMovieCredits(movieID int) (map[string]interface{}, error) {
+	url := fmt.Sprintf("%s/movie/%d/credits?api_key=%s", TMDB_BASE_URL, movieID, c.APIKey)
+	return c.fetchData(url)
+}
+
+// GetGenres fetches the list of movie genres from TMDB.
+func (c *TMDBClient) GetGenres() ([]interface{}, error) {
+	url := fmt.Sprintf("%s/genre/movie/list?api_key=%s", TMDB_BASE_URL, c.APIKey)
+	data, err := c.fetchData(url)
+	if err != nil {
+		return nil, err
+	}
+
+	if genres, ok := data["genres"].([]interface{}); ok {
+		return genres, nil
+	}
+	return nil, fmt.Errorf("could not find 'genres' array in TMDB genres response")
+}
+
+// SearchMovies searches for movies by title from TMDB.
+func (c *TMDBClient) SearchMovies(query string) ([]interface{}, error) {
+	url := fmt.Sprintf("%s/search/movie?api_key=%s&query=%s", TMDB_BASE_URL, c.APIKey, query)
+	data, err := c.fetchData(url)
+	if err != nil {
+		return nil, err
+	}
+
+	if results, ok := data["results"].([]interface{}); ok {
+		return results, nil
+	}
+	return nil, fmt.Errorf("could not find 'results' array in TMDB search response")
+}
+
+// DiscoverMovies discovers movies with filters from TMDB.
+func (c *TMDBClient) DiscoverMovies(genreID, year, sortBy string) ([]interface{}, error) {
+	url := fmt.Sprintf("%s/discover/movie?api_key=%s", TMDB_BASE_URL, c.APIKey)
+	
+	if genreID != "" && genreID != "all" {
+		url += fmt.Sprintf("&with_genres=%s", genreID)
+	}
+	if year != "" && year != "all" {
+		url += fmt.Sprintf("&year=%s", year)
+	}
+	if sortBy != "" {
+		url += fmt.Sprintf("&sort_by=%s", sortBy)
+	} else {
+		url += "&sort_by=popularity.desc"
+	}
+
+	data, err := c.fetchData(url)
+	if err != nil {
+		return nil, err
+	}
+
+	if results, ok := data["results"].([]interface{}); ok {
+		return results, nil
+	}
+	return nil, fmt.Errorf("could not find 'results' array in TMDB discover response")
+}
+
 // fetchData makes an HTTP GET request and unmarshals the JSON response.
 func (c *TMDBClient) fetchData(url string) (map[string]interface{}, error) {
 	resp, err := c.HTTPClient.Get(url)
@@ -74,8 +135,7 @@ func (c *TMDBClient) fetchData(url string) (map[string]interface{}, error) {
 	}
 
 	var data map[string]interface{}
-	if err := json.Unmarshal(body, &data);
-	err != nil {
+	if err := json.Unmarshal(body, &data); err != nil {
 		c.Logger.Error("Failed to unmarshal JSON response from TMDB: %v", err)
 		return nil, fmt.Errorf("failed to unmarshal JSON response: %w", err)
 	}
